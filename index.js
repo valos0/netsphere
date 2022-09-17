60ebd7c699ca48e948e72007ab3d6a4843126f37
valos <valoos@protonmail.com>
2022-09-16 19:44:07 -0700

    0.1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js'),
     Enmap = require('enmap')
 
-const client = new Discord.Client(),
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] }),
     {
         readdirSync
     } = require('fs'),
